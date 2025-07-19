8088f43363e8a0684e7a34bb7a543804fab3cfd8
Diogo <diogoteixeiralucas@pm.me>
2025-07-19 08:35:39 +0100

    Add HomeView and HomeViewController and set as root view

---
+++ fintech/Source/Screens/Home/HomeView.swift
@@ -0,0 +1,18 @@
+import Foundation
+import UIKit
+
+class HomeView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        backgroundColor = .white
+    }
+}
---
+++ fintech/Source/Screens/Home/HomeViewController.swift
@@ -0,0 +1,31 @@
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    let contentView = HomeView()
+  
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func setup() {
+        view.backgroundColor = .red
+        view.addSubview(contentView)
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+}
