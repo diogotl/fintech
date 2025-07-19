1ed384f1a00ec225eacb18c9a5e43aeb97757510
Diogo <diogoteixeiralucas@pm.me>
2025-07-19 07:39:52 +0100

    Initial Commit

---
+++ fintech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fintech
+//
+//  Created by Diogo on 19/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
